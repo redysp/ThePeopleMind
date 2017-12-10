5c45ac19e4c9e4010e9fad9d78b9d4d6d1e33707
unknown <chands10@rpi.edu>
2017-12-10 13:32:37 -0500

    PHP files

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <?php 
+      include('includes/links.inc.php');
+    ?>
+    <script type="text/javascript" src="login.js"></script>
+  </head>
+
+  <body>
+    <?php 
+      include('includes/menu.inc.php'); // include the menu bar and logo
+    ?>
+    <form id="login" name="login" action="#" method="post" onsubmit="return validate(this);">
+        <fieldset> 
+          <legend>Login</legend>
+          <div class="formData">
+                          
+            <label class="field" for="username">Username:</label>
+            <div class="value"><input type="text" size="60" value="" name="username" id="username"/></div>
+            
+            <label class="field" for="password">Password:</label>
+            <div class="value"><input type="text" size="60" value="" name="password" id="password"/></div>
+    
+            <input type="submit" value="save" id="save" name="save"/>
+          </div>
+        </fieldset>
+      </form>
+      
+      <button type="button" onclick="window.location.href='createAccount.php';">Create New Account</button>
+      <button type="button" onclick="window.location.href='forgotPassword.php';">Forgot Password?</button>
+  </body>
+  
+</html>
+
+
---
+++ rules.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The People's Mind</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <?php 
+      include('includes/links.inc.php');
+    ?>
+  </head>
+
+  <body>
+    <?php 
+      include('includes/menu.inc.php'); // include the menu bar and logo
+    ?>
+    <h1>Rules</h1>
+    <ul>
+      <li>The People's Mind helps people solve real life problems with society’s influence</li>
+      <li>Post a question and include up to 6 options to choose from</li>  
+      <li>Vote your opinion on an issue by selecting one of the options</li>
+      <li>In order to comment on an issue, you must vote</li>
+      <li>Want to keep your identity secret? Post your question in the private section (comments are disabled in the private section).</li>
+    </ul>
+  </body>
+  
+</html>
+
+
