fc2e04e254db3a94a318e7c8a201eb55d3df15ac
unknown <chands10@rpi.edu>
2017-12-10 13:21:56 -0500

    Changed private to php

--- private.php
+++ private.php
@@ -3,10 +3,15 @@
   <head>
     <title>The People's Mind</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="stylesheet" type="text/css" href="resources/styles.css"/>
+    <?php 
+      include('includes/links.inc.php');
+    ?>
   </head>
 
   <body>
+    <?php 
+      include('includes/menu.inc.php'); // include the menu bar and logo
+    ?>
     <h1>Private Section</h1>
     <p>All votes and debates here are anonymous</p>
     <h2>Vote:</h2>
