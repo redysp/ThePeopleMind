f1558fe8f0386fae2faeac8b9d953a5e83722df2
unknown <chands10@rpi.edu>
2017-12-10 19:32:12 -0500

    Changed button location

--- rules.php
+++ rules.php
@@ -18,10 +18,10 @@
     <div id="instructions">(Click to see rules)</div>
     <ul id="rulesList">  <!-- Hovering over any list item will make the text blue -->
       <li>The People's Mind helps people solve real life problems with society’s influence</li>
-      <li>Post a question and include options to choose from</li>  
+      <li>Post a question and include up to 6 options to choose from</li>  
       <li>Vote your opinion on an issue by selecting one of the options</li>
       <li>In order to comment on an issue, you must vote</li>
-      <li>Want to keep your identity secret? Post your question in the private section (future development).</li>
+      <li>Want to keep your identity secret? Post your question in the private section (comments are disabled in the private section).</li>
     </ul>
     <?php 
       include('includes/footer.inc.php');
