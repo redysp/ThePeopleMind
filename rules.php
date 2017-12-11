1469d4b560bb38097a2258a7122c3ebbf7c9da4e
Carlos Power <chefpower25@gmail.com>
2017-12-10 19:27:50 -0500

    Add files via upload

--- rules.php
+++ rules.php
@@ -4,30 +4,26 @@
     <title>The People's Mind: Rules</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <script type="text/javascript" src="resources/jquery-3.1.1.min.js"></script>
-    <script type="text/javascript" src="rules.js"></script>
-    <?php 
+    <?php
       include('includes/links.inc.php');
     ?>
   </head>
 
   <body>
-    <?php 
+    <?php
       include('includes/menu.inc.php'); // include the menu bar and logo
     ?>
     <h1 id="rules">Rules</h1>
-    <div id="instructions">(Click to see rules)</div>
     <ul id="rulesList">  <!-- Hovering over any list item will make the text blue -->
       <li>The People's Mind helps people solve real life problems with society’s influence</li>
-      <li>Post a question and include options to choose from</li>  
+      <li>Post a question and include up to 6 options to choose from</li>
       <li>Vote your opinion on an issue by selecting one of the options</li>
       <li>In order to comment on an issue, you must vote</li>
-      <li>Want to keep your identity secret? Post your question in the private section (future development).</li>
+      <li>Want to keep your identity secret? Post your question in the private section (comments are disabled in the private section).</li>
     </ul>
-    <?php 
+    <?php
       include('includes/footer.inc.php');
     ?>
   </body>
-  
+
 </html>
-
-
